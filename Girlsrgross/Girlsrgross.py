ab7ada285da5f1e6d5eb75fc0ae22e3a52f40d80
ZombieBatman <zombiebatman140@gmail.com>
2017-10-13 21:10:58 -0500

    Chang1

--- Girlsrgross/Girlsrgross.py
+++ Girlsrgross/Girlsrgross.py
@@ -12,7 +12,7 @@ class Girlsrgross:
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say("MY FINAL FORM IS UNLEASHING")
+        await self.bot.say("MY FINAL FORM IS UNLEASHING @ian#9201")
 
 def setup(bot):
     bot.add_cog(Girlsrgross(bot))
