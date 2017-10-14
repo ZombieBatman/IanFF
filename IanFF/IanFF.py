b43579878b503dd8422e4b09e60296bfb19aae93
ZombieBatman <zombiebatman140@gmail.com>
2017-10-13 21:17:50 -0500

    Cahanged Name

--- IanFF/IanFF.py
+++ IanFF/IanFF.py
@@ -1,18 +1,25 @@
 import discord
 from discord.ext import commands
 
-class Girlsrgross: 
+class IanFF: 
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
+		self.target = Nun
 
     @commands.command()
-    async def girlsrgross(self):
+    async def IanFF(self):
         """This does stuff!"""
 
         #Your code will go here
         await self.bot.say("MY FINAL FORM IS UNLEASHING @ian#9201")
+	@commands.command()	
+	async def SetTarget(self, var1):
+	"""This Does Things!"""
+	self.target = var1
+	#BotTalking
+	await. self.bot.say("Target has been send" + self.target)
 
 def setup(bot):
-    bot.add_cog(Girlsrgross(bot))
+    bot.add_cog(IanFF(bot))
