4cbd57ca374193e8ce1a50dbfcd011722e330b1d
ZombieBatman <zombiebatman140@gmail.com>
2017-10-13 21:30:22 -0500

    Changed to tabs

--- IanFF/IanFF.py
+++ IanFF/IanFF.py
@@ -2,18 +2,18 @@ import discord
 from discord.ext import commands
 
 class IanFF: 
-    """My custom cog that does stuff!"""
+	"""My custom cog that does stuff!"""
 
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 		self.target = Nun
 
-    @commands.command()
-    async def IanFF(self):
-        """This does stuff!"""
+	@commands.command()
+	async def IanFF(self):
+		"""This does stuff!"""
 
-        #Your code will go here
-        await self.bot.say("MY FINAL FORM IS UNLEASHING @ian#9201")
+		#Your code will go here
+	await self.bot.say("MY FINAL FORM IS UNLEASHING @ian#9201")
 	@commands.command()	
 	async def SetTarget(self, var1):
 	"""This Does Things!"""
@@ -22,4 +22,4 @@ class IanFF:
 	await. self.bot.say("Target has been send" + self.target)
 
 def setup(bot):
-    bot.add_cog(IanFF(bot))
+	bot.add_cog(IanFF(bot))
